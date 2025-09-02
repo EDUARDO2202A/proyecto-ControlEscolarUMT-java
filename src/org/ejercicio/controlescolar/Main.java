2816895e5dca0afc2b34e0cc00e970c31c91ba60
EDUARDO2202A <eduardocanche.mcc@gmail.com>
2025-09-02 19:25:36 +0000

    Actividad POO: Controlescolar

---
+++ src/org/ejercicio/controlescolar/Main.java
@@ -0,0 +1,10 @@
+package org.ejercicio.controlescolar;
+
+public class Main {
+    public static void main(String[] args){
+
+        Persona p = new Persona("ANALICIA", 21);
+
+        System.out.println("Hola,"+p+"!");
+    }
+}
